b3fe519e7b7ca6835476e63c8e83dedcc0ca6ef6
Benedikt Fein <fein@fim.uni-passau.de>
2023-03-15 08:15:00 +0100

    refactor: switch to jinja templates

--- artemis_changelog/main.py
+++ artemis_changelog/main.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: EUPL-1.2
 
 import argparse
-import datetime
+import dataclasses
 import logging
 import os.path
 import re
@@ -15,6 +15,7 @@ import git
 import more_itertools
 import semver
 from git.objects.commit import Commit
+from jinja2 import Environment, PackageLoader, Template, select_autoescape
 
 
 class Sections(Enum):
@@ -30,6 +31,9 @@ class Sections(Enum):
                 return r"src/main/resources/config/liquibase/.*"
             case Sections.TEMPLATE:
                 return r"src/main/resources/templates/.*"
+
+    def __str__(self) -> str:
+        return self.value
 
 
 def fetch_repo(url: str, target_path: Path) -> git.Repo:
@@ -96,54 +100,6 @@ def collect_changed_paths(
     return changed_paths
 
 
-def format_commit_message(commit: Commit) -> str:
-    return str(commit.message).splitlines()[0].strip().replace("]", "\\]")
-
-
-def _licence_header() -> str:
-    # avoid reuse being overly eager to parse the strings as licence headers
-    prefix1 = "SPDX-FileCopyrightText"
-    prefix2 = "SPDX-License-Identifier"
-
-    year = datetime.date.today().year
-
-    return (
-        f"// {prefix1}: {year} Artemis Changelog Contributors\n"
-        "//\n"
-        f"// {prefix2}: CC-BY-SA-4.0"
-    )
-
-
-def format_result(
-    latest_release: git.Tag,
-    result: dict[Sections, set[Commit]],
-) -> str:
-    formatted = _licence_header()
-    formatted += f"\n\n= {latest_release.name}\n\n"
-
-    formatted += (
-        "link:https://github.com/ls1intum/Artemis/releases/tag/"
-        f"{latest_release.name}[Full Release Notes]\n\n"
-    )
-
-    for section, commits in result.items():
-        if len(commits) == 0:
-            continue
-
-        formatted += f"== {section.value}\n\n"
-        for commit in commits:
-            formatted += (
-                "* link:https://www.github.com/ls1intum/Artemis/commit/"
-                f"{commit}[{format_commit_message(commit)}]\n"
-            )
-        formatted += "\n\n"
-
-    if sum(map(len, result.values())) == 0:
-        formatted += "No relevant changes.\n"
-
-    return formatted.rstrip() + "\n"
-
-
 def path_from_version(version: semver.VersionInfo) -> Path:
     def format_number(n: int) -> str:
         return f"{n:02}"
@@ -155,58 +111,46 @@ def path_from_version(version: semver.VersionInfo) -> Path:
     )
 
 
+@dataclasses.dataclass
+class Release:
+    version: semver.VersionInfo
+    changes: dict[Sections, set[Commit]]
+
+
 def create_changelogs(
-    output_dir: Path, tags: Iterable[tuple[semver.VersionInfo, git.Tag]]
+    template: Template,
+    output_dir: Path,
+    tags: Iterable[tuple[semver.VersionInfo, git.Tag]],
 ) -> None:
     for after, before in more_itertools.sliding_window(tags, 2):
         output_path = output_dir / path_from_version(after[0])
         if not os.path.exists(output_path):
             os.makedirs(output_path.parent, exist_ok=True)
             changed_paths = collect_changed_paths(after[1], before[1])
-            result = format_result(after[1], changed_paths)
+            release = Release(after[0], changed_paths)
+
             with open(output_path, "w") as f:
-                f.write(result)
-
-
-def _index_header_notes(tag_count: int) -> str:
-    repo_url = "https://github.com/uni-passau-artemis/artemis-changelog"
-    changelog_url = (
-        "https://github.com/uni-passau-artemis/artemis-changelog/tree/main/changelog"
-    )
-
-    return (
-        "This page is automatically generated from the scripts you "
-        f"can find link:{repo_url}[here].\n\n"
-        "[NOTE]\n"
-        "--\n"
-        f"Only the changes from the last {tag_count} updates are shown here.\n"
-        "You can find details about older releases in the "
-        f"link:{changelog_url}[GitHub repository].\n"
-        "--\n"
-    )
+                f.write(template.render(release=release))
 
 
 def create_index(
-    output_dir: Path, tags: list[tuple[semver.VersionInfo, git.Tag]]
+    template: Template, output_dir: Path, tags: list[tuple[semver.VersionInfo, git.Tag]]
 ) -> None:
     output_path = output_dir / "index.adoc"
+    versions = [t[0] for t in tags[:-1]]
 
     with open(output_path, "w") as f:
-        f.write(_licence_header())
-        f.write("\n\n= Artemis Changelog\n")
-        f.write(":icons: font\n")
-        f.write(":toc: left\n\n")
-
-        f.write(_index_header_notes(len(tags)))
-
-        f.write("\n:leveloffset: +1\n\n")
-
-        for version, _ in tags[:-1]:
-            version_path = path_from_version(version)
-            f.write(f"include::{version_path}[]\n")
+        f.write(template.render(releases=versions))
 
 
 def main(output_dir: Path) -> None:
+    env = Environment(
+        loader=PackageLoader("artemis_changelog"),
+        autoescape=select_autoescape(),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+
     target_path = Path("artemis")
     repo = fetch_repo("https://www.github.com/ls1intum/artemis.git", target_path)
     tags = get_release_tags(repo)
@@ -214,8 +158,11 @@ def main(output_dir: Path) -> None:
 
     tags = tags[:20]
 
-    create_changelogs(output_dir, tags)
-    create_index(output_dir, tags)
+    changelog_template = env.get_template("release_notes.adoc.jinja2")
+    create_changelogs(changelog_template, output_dir, tags)
+
+    index_template = env.get_template("index.adoc.jinja2")
+    create_index(index_template, output_dir, tags)
 
 
 def _arg_parser() -> argparse.ArgumentParser:
